36f1efe4806d53044e5f5f63c16300bac04a461a
femshima <49227365+femshima@users.noreply.github.com>
2022-11-07 20:37:13 +0900

    refactor color

--- src/state/color.rs
+++ src/state/color.rs
@@ -2,59 +2,44 @@ use crate::*;
 use lopdf::content::Operation;
 
 #[derive(Debug, Clone)]
-enum ColorSpace {
-    CMYK,
-    RGB,
-    Gray,
-}
-
-#[derive(Debug, Clone)]
-pub struct Color {
-    space: ColorSpace,
-    data: (f32, f32, f32, f32),
+pub enum Color {
+    CMYK(f32, f32, f32, f32),
+    RGB(f32, f32, f32),
+    Gray(f32),
 }
 
 impl Color {
     pub fn new() -> Self {
-        Self {
-            space: ColorSpace::Gray,
-            data: (0., 0., 0., 0.),
-        }
+        Self::Gray(0.)
     }
     pub fn handle_operation(&mut self, operation: &Operation) {
         match operation.operator.as_ref() {
             "g" | "G" => {
                 if let Ok([g]) = operand_to_f32(&operation).as_deref() {
-                    self.space = ColorSpace::Gray;
-                    self.data = (*g, 0., 0., 0.);
+                    *self = Self::Gray(*g);
                 }
             }
             "rg" | "RG" => {
                 if let Ok([r, g, b]) = operand_to_f32(&operation).as_deref() {
-                    self.space = ColorSpace::RGB;
-                    self.data = (*r, *g, *b, 0.);
+                    *self = Self::RGB(*r, *g, *b);
                 }
             }
             "k" | "K" => {
                 if let Ok([c, m, y, k]) = operand_to_f32(&operation).as_deref() {
-                    self.space = ColorSpace::CMYK;
-                    self.data = (*c, *m, *y, *k);
+                    *self = Self::CMYK(*c, *m, *y, *k);
                 }
             }
             "cs" | "CS" => {
                 if let Some(name) = operation.operands.get(0).and_then(|o| o.as_name().ok()) {
                     match name {
                         b"DeviceGray" => {
-                            self.space = ColorSpace::Gray;
-                            self.data = (0., 0., 0., 0.);
+                            *self = Self::Gray(0.);
                         }
                         b"DeviceRGB" => {
-                            self.space = ColorSpace::RGB;
-                            self.data = (0., 0., 0., 0.);
+                            *self = Self::RGB(0., 0., 0.);
                         }
                         b"DeviceCMYK" => {
-                            self.space = ColorSpace::CMYK;
-                            self.data = (0., 0., 0., 0.);
+                            *self = Self::CMYK(0., 0., 0., 1.);
                         }
                         _ => (),
                     }
@@ -67,15 +52,10 @@ impl Color {
         fn eq(v: f32, c: f32) -> bool {
             (v - c).abs() < 1e-6
         }
-        match self.space {
-            ColorSpace::Gray => eq(self.data.0, 1.),
-            ColorSpace::RGB => eq(self.data.0, 1.) && eq(self.data.1, 1.) && eq(self.data.2, 1.),
-            ColorSpace::CMYK => {
-                eq(self.data.0, 0.)
-                    && eq(self.data.1, 0.)
-                    && eq(self.data.2, 0.)
-                    && eq(self.data.3, 0.)
-            }
+        match self {
+            Self::Gray(g) => eq(*g, 1.),
+            Self::RGB(r, g, b) => eq(*r, 1.) && eq(*g, 1.) && eq(*b, 1.),
+            Self::CMYK(c, m, y, k) => eq(*c, 0.) && eq(*m, 0.) && eq(*y, 0.) && eq(*k, 0.),
         }
     }
 }

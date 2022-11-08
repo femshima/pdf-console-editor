f005bc9109f3f3368a38f13b40133b30fa7cf718
femshima <49227365+femshima@users.noreply.github.com>
2022-11-08 10:42:45 +0900

    refactor state

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use lopdf::content::{Content, Operation};
 use lopdf::{Document, Object};
 use pdf_console_editor::*;
@@ -22,90 +20,19 @@ struct Cli {
     output: std::path::PathBuf,
 }
 
-fn extgstate_map(doc: &Document, page_id: (u32, u16)) -> HashMap<&Vec<u8>, GraphicsState> {
-    let mut result = HashMap::new();
-    if let Some(resource_dict) = doc
-        .get_page_resources(page_id)
-        .0
-        .and_then(|resource| resource.get(b"ExtGState").ok())
-        .and_then(|d| d.as_dict().ok())
-    {
-        for (k, v) in resource_dict {
-            dbg!(k, v);
-            let extgstate_dict = v
-                .as_reference()
-                .and_then(|object_id| doc.get_object(object_id))
-                .and_then(|dict| dict.as_dict());
-            if let Some(extgstate) = extgstate_dict
-                .ok()
-                .and_then(|dict| GraphicsState::from_dict(dict).ok())
-            {
-                result.insert(k, extgstate);
-            }
-        }
-    }
-    result
-}
-
-fn process_content(doc: &Document, page_id: (u32, u16), args: &Cli) -> Content {
+fn process_content<F>(doc: &Document, page_id: (u32, u16), f: F) -> Content
+where
+    F: Fn(Operation, &State) -> Vec<Operation>,
+{
     let content_data = doc.get_page_content(page_id).unwrap();
     let content = Content::decode(&content_data).unwrap();
-    let graphics_state_dict = extgstate_map(doc, page_id);
+
+    let mut state = State::new(doc, page_id);
 
     let mut operations: Vec<Operation> = Vec::new();
-
-    let mut graphics_state_stack: Vec<GraphicsState> = Vec::new();
-    let mut graphics_state = GraphicsState::new();
-    let mut path = path::Path::new();
     for operation in content.operations {
-        path.handle_operation(&operation);
-        graphics_state.handle_operation(&operation);
-        // dbg!(&operation);
-        // dbg!(&graphics_state);
-
-        // println!("{},{:?}", operation.operator, operation.operands);
-        match operation.operator.as_ref() {
-            "q" => {
-                graphics_state_stack.push(graphics_state.clone());
-                operations.push(operation);
-            }
-            "Q" => {
-                if let Some(state) = graphics_state_stack.pop() {
-                    graphics_state = state;
-                }
-                operations.push(operation);
-            }
-            "gs" => {
-                if let Some(state) = operation
-                    .operands
-                    .get(0)
-                    .and_then(|o| o.as_name().ok())
-                    .and_then(|name| graphics_state_dict.get(&name.to_vec()))
-                {
-                    graphics_state = state.clone();
-                }
-                operations.push(operation);
-            }
-            "f" | "F" | "f*" => {
-                if args.rectangle && path.is_rect((args.lower_bound_edge, args.upper_bound_edge)) {
-                    operations.push(Operation::new("n", vec![]))
-                } else {
-                    operations.push(operation);
-                }
-            }
-            "TJ" | "Tj" => {
-                if args.colored_text && graphics_state.color.non_stroke.is_white() {
-                    operations.push(Operation::new(
-                        "rg",
-                        vec![Object::from(0.), Object::from(0.), Object::from(1.)],
-                    ));
-                }
-                operations.push(operation);
-            }
-            _ => {
-                operations.push(operation);
-            }
-        }
+        state.handle_operation(&operation);
+        operations.extend(f(operation, &state));
     }
     Content { operations }
 }
@@ -116,7 +43,39 @@ fn main() {
     let pages: Vec<(u32, u16)> = doc.page_iter().collect();
     // let page_id = doc.page_iter().nth(11).unwrap();
     for page_id in pages {
-        let modified_content = process_content(&doc, page_id, &args).encode().unwrap();
+        let modified_content = process_content(&doc, page_id, |operation, state| {
+            match operation.operator.as_ref() {
+                "f" | "F" | "f*" => {
+                    if args.rectangle
+                        && state
+                            .path
+                            .is_rect((args.lower_bound_edge, args.upper_bound_edge))
+                    {
+                        vec![Operation::new("n", vec![])]
+                    } else {
+                        vec![operation]
+                    }
+                }
+                "TJ" | "Tj" => {
+                    if args.colored_text && state.graphics.color.non_stroke.is_white() {
+                        vec![
+                            Operation::new(
+                                "rg",
+                                vec![Object::from(0.), Object::from(0.), Object::from(1.)],
+                            ),
+                            operation,
+                        ]
+                    } else {
+                        vec![operation]
+                    }
+                }
+                _ => {
+                    vec![operation]
+                }
+            }
+        })
+        .encode()
+        .unwrap();
         doc.change_page_content(page_id, modified_content).unwrap();
     }
     doc.compress();
--- src/state.rs
+++ src/state.rs
@@ -1,104 +1,74 @@
-use lopdf::{content::Operation, Dictionary};
+use std::collections::HashMap;
 
-use crate::{operand_to_f32, CoordMatrix};
+use lopdf::{Document, content::Operation};
 
-use self::{color::ColorState, line::Line, text::Text};
+use self::graphics::GraphicsState;
 
-pub mod color;
-pub mod line;
+pub mod graphics;
 pub mod path;
-pub mod text;
 
-#[derive(Debug, Clone)]
-pub struct GraphicsState {
-    //CTM
-    pub ctm: na::Matrix3<f32>,
-
-    //clipping path
-
-    //color space
-    //color
-    pub color: ColorState,
-
-    //text state
-    pub text: Text,
-
-    //line width
-    //line cap
-    //line join
-    //miter limit
-    //dash pattern
-    pub line: Line,
-
-    //rendering intent
-
-    //stroke adjustment
-    pub stroke_adjustment: bool,
-
-    //blend mode
-    //soft mask
-
-    //alpha constant
-    pub alpha_constant_stroke: f32,
-    pub alpha_constant_non_stroke: f32,
-    //alpha source
-    pub alpha_source: bool,
+pub struct State {
+    pub graphics: GraphicsState,
+    pub path: path::Path,
+    graphics_dict: HashMap<Vec<u8>, GraphicsState>,
+    graphics_stack: Vec<GraphicsState>
 }
 
-impl GraphicsState {
-    pub fn new() -> Self {
+impl State {
+    pub fn new(doc: &Document, page_id: (u32, u16)) -> Self {
         Self {
-            ctm: na::Matrix3::identity(),
-            color: ColorState::new(),
-            text: Text::new(),
-            line: Line::new(),
-            stroke_adjustment: false,
-            alpha_constant_stroke: 1.,
-            alpha_constant_non_stroke: 1.,
-            alpha_source: false,
+            graphics: GraphicsState::new(),
+            path: path::Path::new(),
+            graphics_dict: Self::extgstate_map(doc, page_id),
+            graphics_stack:Vec::new()
         }
     }
+    fn extgstate_map(doc: &Document, page_id: (u32, u16)) -> HashMap<Vec<u8>, GraphicsState> {
+        let mut result = HashMap::new();
+        if let Some(resource_dict) = doc
+            .get_page_resources(page_id)
+            .0
+            .and_then(|resource| resource.get(b"ExtGState").ok())
+            .and_then(|d| d.as_dict().ok())
+        {
+            for (k, v) in resource_dict {
+                let extgstate_dict = v
+                    .as_reference()
+                    .and_then(|object_id| doc.get_object(object_id))
+                    .and_then(|dict| dict.as_dict());
+                if let Some(extgstate) = extgstate_dict
+                    .ok()
+                    .and_then(|dict| GraphicsState::from_dict(dict).ok())
+                {
+                    result.insert(k.to_vec(), extgstate);
+                }
+            }
+        }
+        result
+    }
     pub fn handle_operation(&mut self, operation: &Operation) {
-        self.color.handle_operation(&operation);
-        self.text.handle_operation(&operation);
-        self.line.handle_operation(&operation);
+        self.path.handle_operation(&operation);
+        self.graphics.handle_operation(&operation);
         match operation.operator.as_ref() {
-            "cm" => {
-                if let Ok([a, b, c, d, e, f]) = operand_to_f32(operation).as_deref() {
-                    let m: na::Matrix3<f32> = CoordMatrix::new(*a, *b, *c, *d, *e, *f).into();
-                    self.ctm = m * self.ctm;
+            "q" => {
+                self.graphics_stack.push(self.graphics.clone());
+            }
+            "Q" => {
+                if let Some(state) = self.graphics_stack.pop() {
+                    self.graphics = state;
+                }
+            }
+            "gs" => {
+                if let Some(state) = operation
+                    .operands
+                    .get(0)
+                    .and_then(|o| o.as_name().ok())
+                    .and_then(|name| self.graphics_dict.get(&name.to_vec()))
+                {
+                    self.graphics = state.clone();
                 }
             }
             _ => (),
         }
-    }
-    pub fn from_dict(dict: &Dictionary) -> Result<Self, ()> {
-        if !dict
-            .get(b"Type")
-            .and_then(|t| t.as_name_str())
-            .map_or(false, |v| v == "ExtGState")
-        {
-            return Err(());
-        }
-
-        let mut result = Self::new();
-        result.text = Text::from_dict(&dict);
-        result.line = Line::from_dict(&dict);
-        if let Ok(alpha_constant) = dict
-            .get(b"CA")
-            .and_then(|o| o.as_float().or(o.as_i64().map(|v| v as f32)))
-        {
-            result.alpha_constant_stroke = alpha_constant;
-        }
-        if let Ok(alpha_constant) = dict
-            .get(b"ca")
-            .and_then(|o| o.as_float().or(o.as_i64().map(|v| v as f32)))
-        {
-            result.alpha_constant_non_stroke = alpha_constant;
-        }
-        if let Ok(alpha_source) = dict.get(b"TK").and_then(|o| o.as_bool()) {
-            result.alpha_source = alpha_source;
-        }
-        Ok(result)
     }
 }
